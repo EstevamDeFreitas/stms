04ec9652b025b0a4c0ece747a74a5b9df29bc889
Estevam <estevamdefreitas37@gmail.com>
2022-01-17 20:57:22 -0300

    Added user and loan services

--- src/app/dashboard/components/shared/navbar/navbar.component.ts
+++ src/app/dashboard/components/shared/navbar/navbar.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { LoanService } from 'src/app/dashboard/services/loan.service';
+import { UserService } from 'src/app/dashboard/services/user.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, OnChanges {
 
-  constructor() { }
+  username: string = '';
+
+  credits : number = 0;
+
+  loans: number = 0;
+
+  constructor(private userService: UserService, private loanService: LoanService) { }
 
   ngOnInit(): void {
+    this.updateUserInfo();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+      this.updateUserInfo();
+  }
+
+  updateUserInfo(){
+    this.userService.getUserInfo().subscribe({
+      next: (n) => { this.username = n.user.username; this.credits = n.user.credits; }
+    })
+    
+    this.loanService.getMyLoans().subscribe({
+      next: (n: { loans: any[]; }) => {
+        let temp = 0;
+        
+        n.loans.forEach(loan => {
+          temp += loan.repaymentAmount;
+        });
+
+        this.loans = temp;
+      }
+    })
   }
 
 }
